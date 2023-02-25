a031bbf092f7fdf65dd52b2b37dddd48256a8073
Utkarsh Singh <83408377+utkarsh7370@users.noreply.github.com>
2023-02-26 01:35:32 +0530

    Css update

--- src/components/maincontent/Banner/Banner.js
+++ src/components/maincontent/Banner/Banner.js
@@ -4,49 +4,28 @@ import "./Banner.css";
 
 function Banner() {
   return (
-    <div className="banner">
-      <div className="banner__search">
-        <h1 className="banner__title">
-          Linkup with Creators <br />A new way to discover creators,
-          influencers, or makers
-        </h1>
-        <div className="banner__find">
-          <input
-            className="banner__input"
-            placeholder="Enter platform, keywords, niches or categories"
-          ></input>
-          <button className="banner__button">Go</button>
+    <div className="container mt-5">
+      <div className="banner row mt-2">
+        <div className="banner__search col-lg-5 col-md-10 col-10 offset-1">
+          <h1 className="banner__title">
+            Linkup with Creators <br />A new way to discover creators,
+            influencers, or makers
+          </h1>
+          <div className="banner__find">
+            <input
+              className="banner__input"
+              placeholder="Enter platform, keywords, niches or categories"
+            ></input>
+            <button className="bannerButton">Go</button>
+          </div>
+        </div>
+        <div className="banner__card col-lg-5 col-md-10 col-10 offset-1">
+          <Card1 />
+          <Card1 />
+          <Card1 />
         </div>
       </div>
-      <div className="banner__card">
-        <Card1 />
-        <Card1 />
-        <Card1 />
-      </div>
     </div>
-
-    // <div className="banner">
-    //   <div className="row">
-    //     <div className="col-lg-5 col-md-10 col-10 offset-lg-1 offset-md-1 offset-10">
-    //       <h1 className="banner__title">
-    //         Linkup with Creators <br />A new way to discover creators,
-    //         influencers, or makers
-    //       </h1>
-    //       <div className="banner__find">
-    //         <input
-    //           className="banner__input"
-    //           placeholder="Enter platform, keywords, niches or categories"
-    //         />
-    //         <button className="banner__button">Go</button>
-    //       </div>
-    //     </div>
-    //     <div className="col-lg-5 col-md-10 col-10 offset-lg-1 offset-md-1 offset-10">
-    //       <Card1 />
-    //       <Card1 />
-    //       <Card1 />
-    //     </div>
-    //   </div>
-    // </div>
   );
 }
 
