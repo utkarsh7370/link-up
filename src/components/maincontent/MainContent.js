bc8cf61fa8817d96200c7a96f4223cdb192cc055
pratik <pratik@pop-os.localdomain>
2023-02-26 00:49:39 +0530

    added cart

--- src/components/maincontent/MainContent.js
+++ src/components/maincontent/MainContent.js
@@ -4,11 +4,8 @@ import Banner from "./Banner/Banner";
 import "./MainContent.css";
 import CategoriesRow from "./Row/CategoriesRow";
 import BrandsRow from "./Row/BrandsRow";
-<<<<<<< HEAD
 import FeatureList from "./Features/FeatureList";
-=======
-import Footer from "./Footer/Footer"
->>>>>>> c2113b94dffa4b147eb839149fd72af9cc56f0d8
+import Footer from "./Footer/Footer";
 
 function MainContent() {
   return (
@@ -18,7 +15,7 @@ function MainContent() {
       <BrandsRow />
       <CategoriesRow />
       <FeatureList />
-      <Footer/>
+      <Footer />
     </div>
   );
 }
