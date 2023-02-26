a606e78a47bd16461bfc8f1a362262328386eb41
Utkarsh Singh <83408377+utkarsh7370@users.noreply.github.com>
2023-02-26 13:50:06 +0530

    final

--- src/components/Individual/Socio/Socio.js
+++ src/components/Individual/Socio/Socio.js
@@ -1,27 +1,39 @@
-import React from 'react'
+import React from "react";
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { IconName } from "react-icons/fa";
 import { FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa";
-import './Socio.css'
+import "./Socio.css";
 
 const Socio = () => {
-    return (
-        <div>
-            <div className="social-menu">
-                <ul>
-                    <li><a href="https://www.facebook.com/"><i class="fab  fa-facebook"></i>
-                    </a></li>
-                    <li><a href="https://www.instagram.com/" target="blank"><i className="fab fa-instagram" /></a></li>
-                    <li><a href="https://www.twitter.com/" target="blank"><i className="fab fa-twitter" /></a></li>
-                    <li><a href="https://www.linkedin.com/"><i className="fab fa-linkedin" /></a></li>
-                </ul>
-            </div >
-        </div >
+  return (
+    <div>
+      <div className="social-menu">
+        <ul>
+          <li>
+            <a href="https://www.facebook.com/">
+              <i class="fab  fa-facebook"></i>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.instagram.com/" target="blank">
+              <i className="fab fa-instagram" />
+            </a>
+          </li>
+          <li>
+            <a href="https://www.twitter.com/" target="blank">
+              <i className="fab fa-twitter" />
+            </a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/">
+              <i className="fab fa-linkedin" />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-
-    )
-}
-
-
-export default Socio
+export default Socio;
